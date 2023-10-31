2c193b18c61240dcb6e0659c42a2f09db35784b1
StinflixWork <stinflix.frontsv@gmail.com>
2023-10-31 13:00:05 +0200

    add: prettier & alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			assets: '/src/assets',
+			components: '/src/components',
+			containers: '/src/containers',
+			services: '/src/services',
+			store: '/src/store',
+			utils: '/src/utils'
+		}
+	}
 })
